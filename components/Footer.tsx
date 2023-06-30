e713eee16faf5fe4619e747434b481378782b8ed
ramontavarez <ramonbardic@gmail.com>
2023-06-30 16:28:05 -0300

    fix: footer name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -73,7 +73,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="flexBetween footer_copyright">
-        <p>@ 2023 Flexibble. All rights reserved</p>
+        <p>@ 2023 Dribbbou. All rights reserved</p>
         <p className="text-gray">
           <span className="text-black font-semibold">10,214</span> projects
           submitted
