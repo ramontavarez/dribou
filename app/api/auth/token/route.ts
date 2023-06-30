7059657a532778356ee1247cbd8f3ab3ec5ccdf6
ramontavarez <ramonbardic@gmail.com>
2023-06-30 15:44:51 -0300

    fix: build

--- app/api/auth/token/route.ts
+++ app/api/auth/token/route.ts
@@ -1,9 +1,10 @@
-import { getToken } from "next-auth/jwt"
-import { NextRequest, NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+const secret = process.env.NEXTAUTH_SECRET;
 
 export async function GET(req: NextRequest) {
-  // if using `NEXTAUTH_SECRET` env variable, we detect it, and you won't actually need to `secret`
-  const token = await getToken({ req, raw: true })
+  const token = await getToken({ req, secret, raw: true });
 
-  return NextResponse.json({ token }, {status: 200});
-}
+  return NextResponse.json({ token }, { status: 200 });
+} 
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* @ts-expect-error Server Component */}
         <Navbar />
         <main>{children}</main>
         <Footer />
